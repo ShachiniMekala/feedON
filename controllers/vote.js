e02c491b0e3d7a33b115fce9f760fa342a44b619
Akhila Suraj <akhila.abesinghe@gmail.com>
2020-05-15 02:02:25 +0530

    server_edited

--- controllers/vote.js
+++ controllers/vote.js
@@ -18,9 +18,9 @@ exports.sugView = async (req, res) => {
 }
 
 exports.castingVote = (req, res) => {
-    console.log(req.body.id);
-    console.log(req.body.selectedOption);
-    console.log(req.body.comment);
+    console.log(req["id"]);
+    console.log(req["selectedOption"]);
+    console.log(req["comment"]);
     try {
         //console.log(req.body.id);
         var sugID=req.body.id;
